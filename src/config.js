7d055f6f87e5548dc2143e3ef9b1c52e6d3a8cb8
jannatsk44 <jannatiqbalsk@gmail.com>
2025-04-15 22:09:47 +0530

    Added Company filter and other minute changes

--- src/config.js
+++ src/config.js
@@ -6,5 +6,5 @@ const API_BASE_URL = 'https://compiler-backend.iqratechnology.com:10443/api';
 const CLIENT_ID = '1ff06c76b637c5f12138528d188cef03';
 const CLIENT_SECRET = '14b98ef7dd6ece22d5b93aa80d4ebf1dec5b7b6342dee4d964bbd92c1fea0e9a';
 
-// Export the variables so they can be imported elsewhere
+
 export { API_BASE_URL, CLIENT_ID, CLIENT_SECRET };
