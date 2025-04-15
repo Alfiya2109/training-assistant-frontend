fba51ea71452f90455085baa6561c9d5d9378cb3
jannatsk44 <jannatiqbalsk@gmail.com>
2025-04-15 22:32:22 +0530

    test

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@
 const API_BASE_URL = 'https://compiler-backend.iqratechnology.com:10443/api';
 const CLIENT_ID = '1ff06c76b637c5f12138528d188cef03';
 const CLIENT_SECRET = '14b98ef7dd6ece22d5b93aa80d4ebf1dec5b7b6342dee4d964bbd92c1fea0e9a';
-
+//test
 
 export { API_BASE_URL, CLIENT_ID, CLIENT_SECRET };
