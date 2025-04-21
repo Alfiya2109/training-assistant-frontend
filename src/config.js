fa0b35ab4b8db48f852c23bcc502f2920c72aab7
SaadKhan665 <saadneo12@gmai.com>
2025-04-21 18:23:22 +0530

    added test url to config

--- src/config.js
+++ src/config.js
@@ -2,14 +2,16 @@
 
 // Define your configuration variables here
 // const API_BASE_URL = 'http://127.0.0.1:8000/api';
-const API_BASE_URL = 'https://compiler-backend.iqratechnology.com:10443/api';
+// const API_BASE_URL = 'https://compiler-backend.iqratechnology.com:10443/api';
+const API_BASE_URL = 'https://test-compiler-backend.iqratechnology.com:10444/api';
 const CLIENT_ID = '1ff06c76b637c5f12138528d188cef03';
 const CLIENT_SECRET = '14b98ef7dd6ece22d5b93aa80d4ebf1dec5b7b6342dee4d964bbd92c1fea0e9a';
 
 
+
+
 // const API_MEDIA_URL = 'http://127.0.0.1:8000/';
-
-
-const API_MEDIA_URL = 'https://compiler-backend.iqratechnology.com:10443/';
+// const API_MEDIA_URL = 'https://compiler-backend.iqratechnology.com:10443/';
+const API_MEDIA_URL = 'https://test-compiler-backend.iqratechnology.com:10444/';
 
 export { API_BASE_URL, CLIENT_ID, CLIENT_SECRET, API_MEDIA_URL };
